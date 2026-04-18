bc1ae83d99db8ef0383e59e14e412f57d570c121
Thalio-Toledo <thalio.dev@gmail.com>
2026-04-18 14:13:30 -0300

    fixed environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   //URLS
-  baseUrl: 'https://localhost:7085/',
-  url: 'https://localhost:7085/',
+  //baseUrl: 'https://localhost:7085/',
+  baseUrl: 'https://gvp-api-b3esheenekg0h0ca.brazilsouth-01.azurewebsites.net',
+  //url: 'https://localhost:7085/',
+  url: 'https://gvp-api-b3esheenekg0h0ca.brazilsouth-01.azurewebsites.net',
 };
